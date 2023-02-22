9cd53fabd796d86aace034dc9749b1d7989e8e15
yingcg <yingchiugoh@gmail.com>
2023-02-22 21:14:33 +1300

    unit test

---
+++ TDDpractice/TDDpracticeTest/AwesomeSauceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UnitTestLearning;
+namespace UnitTestLearningTests
+{
+    [TestClass]
+    public class AwesomeSauceTest
+    {
+        [TestMethod]
+        public void IsSauceAwesomeTest()
+        {
+            var testInstance = new AwesomeSauce();
+            testInstance.Sauces.Add("Tobasco");
+            testInstance.Sauces.Add("Chili and Lime");
+            testInstance.Sauces.Add("Tomyam");
+
+            // expect true
+            Assert.IsTrue(testInstance.IsSauceAwesome("Chili and Lime"));
+
+            // expect false
+            Assert.IsFalse(testInstance.IsSauceAwesome("Chili and Lame"));
+        }
+    }
+}
